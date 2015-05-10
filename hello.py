b1aa08adb824dc5d7c983ce985d7fc846c5d1ab8
JD <test.machinery@gmail.com>
2015-05-10 20:13:57 +0200

    base code

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
@@ -10,6 +10,12 @@ app.config.update(
 def home():
     return render_template("home.html")
 
+@app.route('/takeCode')
+def takeCode():
+    takeCode_request = {}
+    takeCode_request['code'] = request.args.get('code','')
+    #return abort(500)
+    return json.dumps({'status': 'ok'})
+
 if __name__ == "__main__":
     app.run(debug=True)
-
