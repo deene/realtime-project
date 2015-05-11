7cbfec1dac603282ab29082a77437c8381c2f891
JD <jd.regreg@gmail.com>
2015-05-11 08:42:11 +0200

    the Pusher code is shaping up

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, request, json
+from pusher import Pusher
+
+pusher = Pusher(
+  app_id=u'119338',
+  key=u'fb391863deb66c13d8c4',
+  secret=u'c65b995116f79406534e'
+)
 
 app = Flask(__name__)
 
@@ -14,6 +21,7 @@ def home():
 def takeCode():
     takeCode_request = {}
     takeCode_request['code'] = request.args.get('code','')
+    pusher.trigger(u'test_channel', u'my_event', {u'message': u'hello world'})
     #return abort(500)
     return json.dumps({'status': 'ok'})
 
